e7fc598db037a5afa16b07453915a6a1fd56a66e
bakka22 <abobakereltom@gmail.com>
2024-08-21 15:29:30 +0200

    S

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" FIFO Replacment policy """
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class FIFOCache(BasicCache):
+    """ Caching system that implements FIFO Replacment policy """
+
+    def __init__(self):
+        """ initialize an instance of the class """
+        super().__init__()
+        self.key_q = []
+
+    def put(self, key, item):
+        """ assign a value to a key in self.cache_data """
+        if not key or not item:
+            return
+        if key not in self.key_q:
+            self.key_q.append(key)
+        self.cache_data[key] = item
+        if len(self.key_q) > self.MAX_ITEMS:
+            dis = self.key_q.pop(0)
+            print(f"DISCARD: {dis}")
+            del self.cache_data[dis]
+
+    def get(self, key):
+        """ retrieve a value of a key from self.cache_data """
+        if not key:
+            return None
+        return self.cahce_data[key]
