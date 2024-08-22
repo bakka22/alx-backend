1341ac5c57d70bf1005ece97edf6d83b4bbf2720
bakka22 <abobakereltom@gmail.com>
2024-08-22 02:26:06 +0200

    s

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,7 +4,7 @@ BasicCache = __import__('0-basic_cache').BasicCache
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BasicCache):
+class LIFOCache(BasicCache):
     """ Caching system that implements FIFO Replacment policy """
 
     def __init__(self):
@@ -16,14 +16,16 @@ class FIFOCache(BasicCache):
         """ assign a value to a key in self.cache_data"""
         if not key or not item:
             return
+        self.cache_data[key] = item
         if key not in self.key_q:
             self.key_q.append(key)
-        self.cache_data[key] = item
-        if len(self.key_q) > BaseCaching.MAX_ITEMS:
-            dis = self.key_q.pop(0)
-            print(f"DISCARD: {dis}")
-            del self.cache_data[dis]
-
+            if len(self.key_q) > BaseCaching.MAX_ITEMS:
+                dis = self.key_q.pop(-2)
+                print(f"DISCARD: {dis}")
+                del self.cache_data[dis]
+        else:
+            self.key_q.append(key)
+            self.key_q.remove(key)
 
     def get(self, key):
         """ retrieve a value of a key from self.cache_data """
